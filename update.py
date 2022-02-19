7af22d6ba5905ff7a3310758476e0a078e46bb8a
ajayk007 <92924618+ajayk007@users.noreply.github.com>
2022-02-19 17:47:51 +0530

    Update update.py

--- update.py
+++ update.py
@@ -8,14 +8,16 @@ class Manager():
   
     
     def process(self):
-        with open("../" + "release.json") as json_file:
+        with open("../" + "UI_release" +"/UI_release.json") as json_file:
             release = json.load(json_file)
         release["last_version"] = release["next_version"]
         release["next_version"] = self.increment_version(release["next_version"])
+        with open("../" + "UI_release" + "/UI_release.json", 'w') as outfile:
+            json.dump(release, outfile, indent=2)
     
     def increment_version(self, version):
         version = version.split('.')
-        version[2] = str(int(version[2]) + 1)
+        version[1] = str(int(version[1]) + 1)
         return '.'.join(version)
 if __name__ == '__main__':
     m = Manager()
