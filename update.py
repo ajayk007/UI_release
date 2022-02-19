58e712444c314a9eb2e327c852727db47476e345
ajayk007 <92924618+ajayk007@users.noreply.github.com>
2022-02-19 17:38:40 +0530

    Update update.py

--- update.py
+++ update.py
@@ -3,12 +3,20 @@ import yaml
 import json
 
 class Manager():
-    def __init__(self, branch) -> None:
-        self.branch = branch
+    
+    def __init__(self) -> None
+  
+    
+    def process(self):
+        with open("../" + "release.json") as json_file:
+            release = json.load(json_file)
+        release["last_version"] = release["next_version"]
+        release["next_version"] = self.increment_version(release["next_version"])
     
     def increment_version(self, version):
-        with open("../" + self.branch + "/release.json") as json_file:
-            release = json.load(json_file)
         version = version.split('.')
         version[2] = str(int(version[2]) + 1)
         return '.'.join(version)
+if __name__ == '__main__':
+    m = Manager()
+    m.process()
